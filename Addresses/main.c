9d0c17a76b21a6b17a3e8686e03b78e1326d1842
Edwin Cardenas <eacardenase@gmail.com>
2024-12-30 09:53:41 -0500

    feat: pointers basics

--- Addresses/main.c
+++ Addresses/main.c
@@ -8,7 +8,15 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int i = 17;
+    int *addressOfI = &i;
+    
+    printf("i stores it's value %d at %p\n", i, &addressOfI);
+    printf("This function starts at %p\n", main);
+    printf("The int stored at addressOfI is %d\n", *addressOfI);
+    
+    *addressOfI = 89;
+    printf("Now i is %d\n", i);
+    
     return 0;
 }
